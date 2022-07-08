593630178b5542efeee1f79d248a0150cefc50da
deep mind <deepmind2021@gmail.com>
2022-07-08 15:09:44 -0400

    ejercicio 2 sintaxis

---
+++ 02-sintaxis/index.js
@@ -0,0 +1,15 @@
+var nombre= "cesar";
+var edad = 28;
+var eresdesarrollador= true;
+var fechanac = new Date(1994,04,30)
+var libro_favorito = {
+    titulo: "Fortaleza Digital",
+    autor: "Dan Brown",
+    fecha: 1998,
+    url: "https://usercontent.one/wp/www.puro-geek.com/wp-content/uploads/2021/11/La-Fortaleza-Digital-Dan-Brown.pdf?media=1630018077"
+}
+
+const lista = [nombre, edad, eresdesarrollador, fechanac, libro_favorito]
+
+
+console.log(lista);
