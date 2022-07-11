330ec5b620bbfd7da276b6cd530e5cd21304bf17
deep mind <deepmind2021@gmail.com>
2022-07-11 15:37:16 -0400

    ejercicios de numeros

---
+++ 05-numeros/numeros.js
@@ -0,0 +1,17 @@
+// Una variable que contenga tu altura en centímetros (entero)
+const altura = 167;
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+const metrosAltura = 1.67;
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+const peso = 67;
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+const redondeadaAltura = Math.round(metrosAltura);
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+const  rondondearPeso = Math.floor(peso);
+
+// Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+const igualar = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
