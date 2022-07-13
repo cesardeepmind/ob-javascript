5fa28f502fe40ffda4fad19ae594c8808f3c4d27
deep mind <deepmind2021@gmail.com>
2022-07-13 09:48:35 -0400

    ejercicios de fechas en JS

---
+++ 07-sets-y-objetos/fechas.js
@@ -0,0 +1,9 @@
+const hoy = new Date()
+
+const fechanacimiento = new Date(1994, 04, 30)
+
+const comparacion = hoy > fechanacimiento
+
+const diaNacimiento = fechanacimiento.getDate()
+const mesNacimiento = fechanacimiento.getMonth()
+const anyoNacimiento = fechanacimiento.getFullYear()
